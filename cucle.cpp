d119fdc82f9c998245603b43db2713f065971233
EkaterinaRusakova17 <145272537+EkaterinaRusakova17@users.noreply.github.com>
2023-10-11 14:51:51 +0700

    Update cucle.cpp

--- cucle.cpp
+++ cucle.cpp
@@ -32,20 +32,19 @@ int main()
 
   Gr = ( (Tv - Tc) / 273.0 ) * ( (g * pow (l, 3.0) ) / (v * v) );
 
-  while ( (W > 0.4) && (W < 11.0) ); {
-  Re = (W * a) / v;
-  cout << "Re=" << Re << endl;
-
-}
+  while ( (W > 0.4) && (W < 11.0) ) {
+    Re = (W * a) / v;
+    cout << "Re=" << Re << endl;
+  }
 
   if (Re < 2200.0) {
-  Nu = 0.13 * pow (Re, 0.33) * pow (Gr, 0.1);
+    Nu = 0.13 * pow (Re, 0.33) * pow (Gr, 0.1);
   } else {
-  Nu = 0.018 * pow (Re, 0.88);}
-  cout << "Nu=" << Nu << endl;
+    Nu = 0.018 * pow (Re, 0.88);
+    cout << "Nu=" << Nu << endl;
+  }
 
   Q = 4.0 * liamda * l * Nu * (Tv - Tc);
-
   cout << "Q= " << Q << endl;
 
   return 0;
